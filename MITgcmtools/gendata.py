67abadc7ebb2866e0a47aef9bd3adb28f321dfc6
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2020-12-21 13:00:03 -0500

    better read/write bin

--- MITgcmtools/gendata.py
+++ MITgcmtools/gendata.py
@@ -41,27 +41,22 @@ def _get_np_from_xr(arr):
         return arr.values
     return arr
 
-def writeIEEEbin(filename, arr, precision=64):
-    """Write a numpy array to a IEEE (Big-Endian) binary file."""
+def writebin(filename, arr, precision=64, endianness='>'):
+    """Write an array to a binary file in F-order."""
     arr = _get_np_from_xr(arr)
-    # Python is in C-layout (X, Y, Z), so we have to flip to F-layout (Z, Y, X)
-    if arr.ndim == 2: 
-        arr = arr.T
-    elif arr.ndim == 3:
-        arr = arr.swapaxes(0, 2)
     # Precision is in bits, the dtype is in Bytes. > : IEEE, f : float, 4/8 : precision
-    print(f"Writing data of shape {arr.shape} to file {filename} with precision of {precision} bits")
-    arr.astype(">f{:d}".format(precision // 8)).tofile(str(filename))
+    print(f"Writing data of shape {arr.shape} to file {filename} with precision of {precision} bits using '{endianness}'")
+    with open(filename, 'wb') as f:
+        f.write(arr.astype("{0}f{1:d}".format(endianness, precision // 8)).tobytes('F'))
 
-
-def write_concatenate(filename, *arrs, precision=64):
+def write_concatenate(filename, *arrs, precision=64, endianness='>'):
     """Write multiple numpy arrays to a single file."""
     arrays = [_get_np_from_xr(arr) for arr in arrs]
     conc = np.stack(arrays, axis=-1)
-    writeIEEEbin(filename, conc, precision=precision)
+    writeIEEEbin(filename, conc, precision=precision, endianness=endianness)
 
-def readIEEEbin(filename, shape, precision=64):
-    """Read a IEEE (Big-Endian) binary file.
+def readbin(filename, shape, precision=64, endianness='>'):
+    """Read a binary file.
 
     Arguments:
         filename -- Filename to read
@@ -71,7 +66,7 @@ def readIEEEbin(filename, shape, precision=64):
         precision -- number of bits for each float (default: {64} (8 B))
     """
     arr = np.fromfile(
-        str(filename), dtype=np.dtype(">f{:d}".format(precision // 8))
+        str(filename), dtype=np.dtype("{0}f{1:d}".format(endianness, precision // 8))
     ).reshape(shape[::-1])
     if arr.ndim == 2:
         return arr.T
